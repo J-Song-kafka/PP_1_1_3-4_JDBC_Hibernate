a6cc445d7b2bb0e8d3f2f80c853df86d7dd7ef07
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    Сделала задачу ураааааааааааааааааа

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Max", "Satyshev", (byte) 22);
+        userService.saveUser("Tatyana", "Samokhina", (byte) 60);
+        userService.saveUser("Larisa", "Pogozheva", (byte) 50);
+        userService.saveUser("Taisiya", "Khalilova", (byte) 26);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbtest", "root", "root");
+            if (!connection.isClosed()) {
+                System.out.println("Connection established");
+            }
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException("Пупупу");
+//            e.printStackTrace();
+        }
+    }
+//    public Connection getConnection() {
+//        return connection;
+//    }
 }
